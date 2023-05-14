6435b1f0e7549048bf26c9afa7267e8e5a980b8a
Valentin <hanolo@yandex.ru>
2023-05-14 14:04:14 +0300

    react v.2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 //Императивный
+import { jsx as _jsx } from "react/jsx-runtime";
+import { jsxs as _jsxs } from "react/jsx-runtime";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-		  Date().split(' ')[3];
-        </a>
-      </header>
-    </div>
-  );
+  return /*#__PURE__*/_jsx("div", {
+    className: "App",
+    children: /*#__PURE__*/_jsxs("header", {
+      className: "App-header",
+      children: [/*#__PURE__*/_jsx("img", {
+        src: logo,
+        className: "App-logo",
+        alt: "logo"
+      }), /*#__PURE__*/_jsxs("p", {
+        children: ["Edit ", /*#__PURE__*/_jsx("code", {
+          children: "src/App.js"
+        }), " and save to reload."]
+      }), /*#__PURE__*/_jsx("a", {
+        className: "App-link",
+        href: "https://reactjs.org",
+        target: "_blank",
+        rel: "noopener noreferrer",
+        children: "Learn React Date().split(' ')[3];"
+      })]
+    })
+  });
 }
-
 export default App;
